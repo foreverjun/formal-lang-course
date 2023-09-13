2ebd9b6ec4c007e7ad99274db81f34e24120dd9b
matvey <kalashnikov.matvey.m@gmail.com>
2023-09-14 00:11:24 +0300

    fixed tests and tests naming

--- tests/test_graph_module.py
+++ tests/test_graph_module.py
@@ -2,17 +2,17 @@ from project import graph_module
 import pathlib
 
 
-def get_graph_metrics_test():
-    nodes_num, edges_num, labels = graph_module.get_graph_metrics_by_name(bzip)
+def test_get_graph_metrics():
+    nodes_num, edges_num, labels = graph_module.get_graph_metrics_by_name("bzip")
     assert nodes_num == 632
     assert edges_num == 556
-    assert labels == {"a", "b"}
+    assert labels == {"a", "d"}
 
 
 # Create and save graph with two cycles. After that load it and check their equality
 
 
-def save_graph_test():
+def test_save_graph():
     graph_module.generate_and_save_two_cycles_graph(4, 5, ("c", "d"), "actual.dot")
     current_dir = pathlib.Path().resolve()
     expected_path = pathlib.Path(current_dir, "expected.dot")
