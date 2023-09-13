7cbb553d7caff038eee4d8377273710447964e5d
matvey <kalashnikov.matvey.m@gmail.com>
2023-09-13 23:19:35 +0300

    update test for multiple os

--- project/graph_module.py
+++ project/graph_module.py
@@ -1,3 +1,5 @@
+from typing import Tuple
+
 import cfpq_data
 import networkx as nx
 
@@ -7,7 +9,7 @@ def get_graph(name: str) -> nx.MultiDiGraph:
     return cfpq_data.graph_from_csv(gr_path)
 
 
-def get_graph_metrics(graph: nx.MultiDiGraph) -> (int, int, set):
+def get_graph_metrics(graph: nx.MultiDiGraph) -> Tuple[int, int, set]:
     nodes_num: int = graph.number_of_nodes()
     edges_num: int = graph.number_of_edges()
     # getting set of edge labels
@@ -17,7 +19,7 @@ def get_graph_metrics(graph: nx.MultiDiGraph) -> (int, int, set):
     return nodes_num, edges_num, labels
 
 
-def get_graph_metrics_by_name(name: str) -> (int, int, set):
+def get_graph_metrics_by_name(name: str) -> Tuple[int, int, set]:
     graph = get_graph(name)
     return get_graph_metrics(graph)
 
@@ -25,7 +27,7 @@ def get_graph_metrics_by_name(name: str) -> (int, int, set):
 # first_cycle_num : The number of nodes in the first cycle without a common node
 # second_cycle_num : The number of nodes in the second cycle without a common node
 def generate_and_save_two_cycles_graph(
-    first_cycle_num: int, second_cycle_num: int, labels: (str, str), path: str
+    first_cycle_num: int, second_cycle_num: int, labels: Tuple[str, str], path: str
 ):
     graph = nx.labeled_two_cycles_graph(
         first_cycle_num, second_cycle_num, labels=labels
